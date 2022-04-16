8c391ec2fcda21162a26a01d678fb34b5e1d4327
Samantha Crespo <samkcrespo@gmail.com>
2022-04-16 16:39:46 -0700

    commit tree 5f974b009f0a645bfe212dc879e143bb85fd2062

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+
+// fetch('http://localhost:3000/dogs', configurationObject)
+// .then(function (response){
+//   return response.json();
+// })
+// .then(function (object) {
+//   console.log(object);
+// })
+// .catch(function (error) {
+//   alert("Bad things! Ragnarok!");
+//   console.log(error.message);
+// });
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: 
+      {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(function (response) {
+      return response.json()})
+      .then(function (object) {
+        document.body.innerHTML = object["id"]
+      })
+      .catch(function (error) {
+        document.body.innerHTML = error.message
+      })
+    }
